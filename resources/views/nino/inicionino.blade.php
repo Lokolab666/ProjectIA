7553d36a0fc24538d3ac2c2976e5a84ebf56c4c1
Lokolab666 <cristian9918@hotmail.com>
2022-09-17 23:13:36 -0600

    Inicio niño

--- resources/views/nino/inicionino.blade.php
+++ resources/views/nino/inicionino.blade.php
@@ -1,5 +1,32 @@
 @extends('app')
 
 @section('content')
-    Inicio Nino
+<div class="position-absolute top-50 start-50 translate-middle">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                <img src="{{ asset('/mono.png') }}" class="card-img-top" alt="Acceder a foto">
+                    <div class="card-body">
+                        <h5 class="card-title">Tomar Fotografía</h5>
+                        <p class="card-text">Accede a tomar una nueva foto</p>
+                        <a href="#" class="btn btn-primary">Tomar foto</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                <img src="{{ asset('/tarjeta.png') }}" class="card-img-top" alt="Acceder a libreria">
+                    <div class="card-body">
+                        <h5 class="card-title">Tarjetas</h5>
+                        <p class="card-text">Mira las tarjetas que has creado</p>
+                        <a href="#" class="btn btn-primary">Ver tarjetas</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 @endsection
